c14d57e943082d3e828b69b4f2bb976a2aec9d98
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-26 00:48:15 +0200

    refactor: rename controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('homepage/index.html.twig', []);
+        return $this->render('home/index.html.twig', []);
     }
 }
