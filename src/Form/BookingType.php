370fd76c21ae431113d5004627593626d1326a5c
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-26 15:07:29 +0200

    feat: enhance form

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,31 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date')
-            ->add('nbPlaces')
-            ->add('name')
-            ->add('email')
-            ->add('phone');
+            ->add('date', type: DateTimeType::class, options: [
+                'label' => 'Date',
+                'data' => new \DateTime(),
+                'required' => true,
+            ])
+            ->add('nbPlaces', options: [
+                'label' => 'Nombre de couverts',
+                'attr' => ['placeholder' => '2'],
+                'required' => true,
+            ])
+            ->add('name', options: [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Votre nom'],
+                'required' => true,
+            ])
+            ->add('email', options: [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Votre email'],
+                'required' => true,
+            ])
+            ->add('phone', options: [
+                'label' => 'Téléphone',
+                'attr' => ['placeholder' => 'Votre numéro de téléphone'],
+                'required' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
