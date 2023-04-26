2b95455d283bd6274cb517eaec4e2dc126d5e8c9
Tommy Dumora <tommydumora@gmail.com>
2023-04-26 17:03:46 +0200

    Update page actu

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class ActualiteController extends AbstractController
 {
     #[Route('/actualite', name: 'app_actualite')]
-    public function index(): Response
+    public function index(ActualiteRepository $actualiteRepository): Response
     {
+        $actualites = $actualiteRepository->findAll();
         return $this->render('actualite/index.html.twig', [
-            'controller_name' => 'ActualiteController'
+            'actualites' => $actualites
+        ]);
+    }
+
+    #[Route('/actualite/{id}/view', name: 'app_actualite_view', methods: ["GET"])]
+    public function view(
+        int $id,
+        ActualiteRepository $actualiteRepository
+    ): Response {
+
+        $actualite = $actualiteRepository->find($id);
+
+        return $this->render("actualite/view.html.twig", [
+            'actualite' => $actualite
         ]);
     }
 }
