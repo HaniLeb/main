054a9c58ea1a07f772f43295d0b53bc21c91e34f
Tommy Dumora <tommydumora@gmail.com>
2023-05-03 12:05:13 +0200

    finition page actu

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Actualite;
+use App\Form\ActualiteType;
 use App\Repository\ActualiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,6 +31,70 @@ class ActualiteController extends AbstractController
         $actualite = $actualiteRepository->find($id);
 
         return $this->render("actualite/view.html.twig", [
+            'actualite' => $actualite
+        ]);
+    }
+
+    #[Route('/actualite/create', name: 'app_actualite_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $actualite = new Actualite();
+        $form = $this->createForm(ActualiteType::class, $actualite);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($actualite);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_actualite');
+        }
+
+        return $this->render('actualite/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/actualite/{id}/delete', name: 'app_actualite_delete', methods: ["POST"])]
+    public function delete(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ActualiteRepository $actualiteRepository
+    ): Response {
+        $token = $request->request->get('token');
+        $actualite = $actualiteRepository->find($id);
+
+        if ($this->isCsrfTokenValid('actualite-' . $actualite->getId(), $token)) {
+            $entityManager->remove($actualite);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_actualite');
+    }
+
+    #[Route('/actualite/{id}/edit', name: 'app_actualite_edit')]
+    public function edit(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ActualiteRepository $actualiteRepository
+    ): Response {
+        $actualite = $actualiteRepository->find($id);
+        $form = $this->createForm(ActualiteType::class, $actualite);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($actualite);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_actualite');
+        }
+
+        return $this->render('actualite/edit.html.twig', [
+            'form' => $form->createView(),
             'actualite' => $actualite
         ]);
     }
